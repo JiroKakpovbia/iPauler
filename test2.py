307133ae1372cd9785bcf84257237c97a5f873c4
ryan <rschindl@uwaterloo.ca>
2022-11-10 00:05:34 -0500

    paul

--- test2.py
+++ test2.py
@@ -1,6 +1,6 @@
 from subprocess import call
 import speech_recognition as sr
-import serial
+#import serial
 #import RPi.GPIO as GPIO    
 # for speech-to-text
 import speech_recognition as sr
@@ -77,105 +77,5 @@ if __name__ == "__main__":
     while ex:
         ai.speech_to_text()
 
-        ##Only respond if awake
-        if ai.awake:
-
-            if ai.mood == 1:
-
-                ## change to mood 2
-                if "mood two" in ai.text:
-                    res = "Now in mood two."
-                    ai.mood = 2
-
-                ## change to mood 3
-                elif "mood three" in ai.text:
-                    res = "Now in mood three."
-                    ai.mood = 3
-
-                ## action time
-                elif "time" in ai.text:
-                    res = ai.action_time()
-
-                ## what day
-                elif "what day" in ai.text:
-                    res = "It's every day bro!"
-
-                ## city
-                elif "city" in ai.text:
-                    res = "England is my city."
-
-                ## number one
-                elif "number one" in ai.text:
-                    res = "I'm number one."
-
-                ## how old
-                elif "how old" in ai.text:
-                    res = "I'm twenty five years old."
-
-                ## boxing record
-                elif "record" in ai.text:
-                    res = "Six and O baby. Hear that? Six and O."
-
-                ## best YouTube boxer
-                elif "best YouTube boxer" in ai.text:
-                    res = "Realy? Really? It's me you dumbass. Don't ask me that again."
-
-                ## your name
-                elif "your name" in ai.text:
-                    res = "I'm the one and only Jake Joseph Paul."
-                
-                ## respond politely
-                elif any(i in ai.text for i in ["thank","thanks"]):
-                    res = np.random.choice(["you're welcome!","anytime!","no problem!","cool!","I'm here if you need me!","mention not"])
-            
-                else:
-                    if ai.text=="ERROR":
-                        res="Sorry, come again?"
-
-##            elif ai.mood == 2:
-##                
-##                ## change to mood 1
-##                if "mood one" in ai.text:
-##                    res = "Now in mood one."
-##                    ai.mood = 1
-##
-##                ## change to mood 3
-##                elif "mood three" in ai.text:
-##                    res = "Now in mood three."
-##                    ai.mood = 3
-
-##            elif ai.mood == 3:
-##                ## change to mood 1
-##                if "mood one" in ai.text:
-##                    res = "Now in mood one."
-##                    ai.mood = 1
-##
-##                ## change to mood 2
-##                elif "mood two" in ai.text:
-##                    res = "Now in mood two."
-##                    ai.mood = 2
-
-            ##Stop running the program if certain keyword is said
-            if any(i in ai.text for i in ["exit","close"]):
-                res = np.random.choice(["Tata","Have a good day","Bye","Goodbye","Hope to meet soon","peace out!"])
-                
-                ex=False
-
-            ## conversation
-            else:   
-                chat = nlp(transformers.Conversation(ai.text), pad_token_id=50256)
-                res = str(chat)
-                res = res[res.find("bot >> ")+6:].strip()
-
-            ai.text_to_speech(res)
-
-
-        ## wake up if name inputted
-        if ai.name in ai.text:
-        
-            res = "Jake Paul is here and ready to rumble"
-            ai.awake = True
-            ai.text_to_speech(res)
-
-    print("----- Closing down Jake Paul -----")
+ 
 
