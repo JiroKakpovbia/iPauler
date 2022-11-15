19379d0f1dc00eaa677779ab67e79cbcfd130654
Ryan <ryansoliveandpineappleshopis@gmail.com>
2022-11-14 22:22:01 -0500

    pygame test

--- test.py
+++ test.py
@@ -1,13 +1,34 @@
 import pyttsx3
 import speech_recognition as sr
-import vlc
+
 import time
+import pygame
 
 
-p = vlc.MediaPlayer("everydaybro.mp4/")
-r = sr.Recognizer
+pygame.init()
+X = 480
+Y = 320
+
+scrn = pygame.display.set_mode((X, Y))
 
 
-p.play()
-
-time.sleep(60)
+run = True
+imp = pygame.image.load("boxer.jpg").convert()
+scrn.blit(imp, (0, 0))
+pygame.display.flip()
+while (run):
+ 
+  # iterate over the list of Event objects
+  # that was returned by pygame.event.get() method.
+    for i in pygame.event.get():
+ 
+        # if event object type is QUIT
+        # then quitting the pygame
+        # and program both.
+        if i.type == pygame.QUIT:
+            status = False
+ 
+    
+ 
+# paint screen one time
+pygame.display.flip()
