d403f28d15257d3f9650af54444eb945dde5f392
Ryan <ryansoliveandpineappleshopis@gmail.com>
2022-11-14 22:35:53 -0500

    pygame test

--- test.py
+++ test.py
@@ -6,29 +6,4 @@ import pygame
 
 
 pygame.init()
-X = 480
-Y = 320
-
-scrn = pygame.display.set_mode((X, Y))
-
-
-run = True
-imp = pygame.image.load("boxer.jpg").convert()
-scrn.blit(imp, (0, 0))
-pygame.display.flip()
-while (run):
- 
-  # iterate over the list of Event objects
-  # that was returned by pygame.event.get() method.
-    for i in pygame.event.get():
- 
-        # if event object type is QUIT
-        # then quitting the pygame
-        # and program both.
-        if i.type == pygame.QUIT:
-            status = False
- 
-    
- 
-# paint screen one time
-pygame.display.flip()
+pygame.display.list_modes()
