a752d8c787d5e3e33f3a00da6030a32ee7a378e2
Ryan <ryansoliveandpineappleshopis@gmail.com>
2022-11-14 22:38:55 -0500

    pygame test

--- test.py
+++ test.py
@@ -2,8 +2,13 @@ import pyttsx3
 import speech_recognition as sr
 
 import time
+import os
+import sys
+os.environ["SDL_VIDEODRIVER"] = "dummy"
 import pygame
 
 
 pygame.init()
+
 pygame.display.list_modes()
+
