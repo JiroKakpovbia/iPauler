f33e881628d2119ed1765926157e4f19832c3c18
Ryan <ryansoliveandpineappleshopis@gmail.com>
2022-11-16 01:52:17 -0500

    a

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import speech_recognition as sr
 import time
 import os
 import sys
-os.environ["SDL_VIDEODRIVER"] = "x11"
+os.environ["SDL_VIDEODRIVER"] = "kmsdrm"
 os.environ['DISPLAY'] = ': 0.0'
 import pygame
 
