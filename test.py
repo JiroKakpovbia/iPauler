7a00725a2499ec20daf21d10af604b88a356fc85
Ryan <ryansoliveandpineappleshopis@gmail.com>
2022-11-16 01:52:47 -0500

    a

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import speech_recognition as sr
 import time
 import os
 import sys
-os.environ["SDL_VIDEODRIVER"] = "kmsdrm"
+os.environ["SDL_VIDEODRIVER"] = "KMS"
 os.environ['DISPLAY'] = ': 0.0'
 import pygame
 
