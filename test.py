59ed575080c860dece2016aa88a668bd32f66e88
Ryan <ryansoliveandpineappleshopis@gmail.com>
2022-11-16 02:29:22 -0500

    a

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import speech_recognition as sr
 import time
 import os
 import sys
-os.environ["SDL_VIDEODRIVER"] = "directfb"
+os.environ["SDL_VIDEODRIVER"] = "X11"
 #os.environ['DISPLAY'] = ': 0.0'
 import pygame
 
