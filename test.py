da6eb04c79cf03a8a36e20e1cd8bba5c6b3a4991
Ryan <ryansoliveandpineappleshopis@gmail.com>
2022-11-16 01:49:23 -0500

    a

--- test.py
+++ test.py
@@ -4,38 +4,38 @@ import speech_recognition as sr
 import time
 import os
 import sys
-#os.environ["SDL_VIDEODRIVER"] = "dummy"
+os.environ["SDL_VIDEODRIVER"] = "x11"
 os.environ['DISPLAY'] = ': 0.0'
-import sdl2
-import sdl2.ext as sdl2ext
+import pygame
 
 
+pygame.init()
 
-sdl2.ext.init()
+
 
 X = 640
 Y = 480
 
-scrn = sdl2.ext.Window("gaming", size=(X, Y))
+scrn = pygame.display.set_mode((X, Y))
 
 
 run = True
-imp = sdl2.ext.image.load_img("boxer.jpg")
-sdl2.surface.SDL_BlitSurface(imp, scrn, 0, 0)
-scrn.refresh()
+imp = pygame.image.load("boxer.jpg").convert()
+scrn.blit(imp, (0, 0))
+pygame.display.flip()
 while (run):
  
   # iterate over the list of Event objects
   # that was returned by pygame.event.get() method.
-    for i in sdl2.SDL_PollEvent():
+    for i in pygame.event.get():
  
         # if event object type is QUIT
         # then quitting the pygame
         # and program both.
-        if i.type == sdl2.sdlttf.TTF_Quit():
+        if i.type == pygame.QUIT:
             status = False
  
     
  
 # paint screen one time
-
+pygame.display.flip()
