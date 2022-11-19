aa4012d84887b02d3df92f0d95315b09f25887d4
ryan <rschindl@uwaterloo.ca>
2022-11-19 18:09:30 -0500

    lights

---
+++ lights.py
@@ -0,0 +1,50 @@
+import RPi.GPIO as GPIO
+from time import sleep
+#disable warnings (optional)
+GPIO.setwarnings(False)
+#Select GPIO Mode
+GPIO.setmode(GPIO.BCM)
+#set red,green and blue pins
+redPin = 12
+greenPin = 19
+bluePin = 13
+#set pins as outputs
+GPIO.setup(redPin,GPIO.OUT)
+GPIO.setup(greenPin,GPIO.OUT)
+GPIO.setup(bluePin,GPIO.OUT)
+
+def turnOff():
+    GPIO.output(redPin,GPIO.HIGH)
+    GPIO.output(greenPin,GPIO.HIGH)
+    GPIO.output(bluePin,GPIO.HIGH)
+    
+def white():
+    GPIO.output(redPin,GPIO.LOW)
+    GPIO.output(greenPin,GPIO.LOW)
+    GPIO.output(bluePin,GPIO.LOW)
+    
+def red():
+    GPIO.output(redPin,GPIO.LOW)
+    GPIO.output(greenPin,GPIO.HIGH)
+    GPIO.output(bluePin,GPIO.HIGH)
+def green():
+    GPIO.output(redPin,GPIO.HIGH)
+    GPIO.output(greenPin,GPIO.HIGH)
+    GPIO.output(bluePin,GPIO.LOW)
+def blue():
+    GPIO.output(redPin,GPIO.HIGH)
+    GPIO.output(greenPin,GPIO.LOW)
+    GPIO.output(bluePin,GPIO.HIGH)
+
+if __name__ == "__main__":
+    run = True
+
+    while run:
+
+        val = input("choose color: ")
+        if(val == 1):
+            red()
+        if(val == 2):
+            green()
+        if(val == 3):
+            blue()
