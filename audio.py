879d954a4f9abe1b660cb5cdc7c3c6a2ec7c75b2
ryan <rschindl@uwaterloo.ca>
2022-11-19 22:58:03 -0500

    lights

--- audio.py
+++ audio.py
@@ -1,30 +1,8 @@
-import speech_recognition as sr
-import pyttsx3
-import os
+
 
 import vlc
-# for text-to-speech
-from gtts import gTTS
 
-# for language model
 
-from playsound import playsound
-
-import threading
-import time
-
-# for data
-import datetime
-import numpy as np
-
-import recording
-import responses
-
-import pygame
-
-import pvporcupine
-
-from pvrecorder import PvRecorder
 
 p = vlc.MediaPlayer("everydaybro.mp4/") 
                 
