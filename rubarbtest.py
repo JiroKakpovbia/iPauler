c2b25410e0aca03e6f75c01477bf4fa4ebaacd63
Ryan <ryansoliveandpineappleshopis@gmail.com>
2022-11-26 02:02:26 -0500

    fix

--- rubarbtest.py
+++ rubarbtest.py
@@ -8,7 +8,7 @@ pygame.init()
 pygame.mixer.init()
 
 result = pygame.mixer.music.load("meme.mp3")
-subprocess.run(["home/se101/Rhubarb-Lip-Sync-1.13.0-Linux/rhubarb -o output.json -f --json-r --pocketSphinx meme.mp3"])
+subprocess.run(["/home/se101/Rhubarb-Lip-Sync-1.13.0-Linux/rhubarb -o output.json -f --json-r --pocketSphinx meme.mp3"])
 
 
 f = open('output.json')
