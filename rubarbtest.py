544382129de1ff6d4f75d7c752e746dcfd761ebf
Ryan <ryansoliveandpineappleshopis@gmail.com>
2022-11-26 04:27:22 -0500

    fix

--- rubarbtest.py
+++ rubarbtest.py
@@ -8,8 +8,22 @@ import json
 pygame.init()
 pygame.mixer.init()
 
-result = pygame.mixer.music.load("meme.mp3")
+X = 480
+Y = 320
 
+    
+screen = pygame.display.set_mode((X, Y))
+
+result = pygame.mixer.music.load("meme.mp3")
+A = pygame.image.load("mouth/A.png").convert()
+B = pygame.image.load("mouth/B.png").convert()
+C = pygame.image.load("mouth/C.png").convert()
+D = pygame.image.load("mouth/D.png").convert()
+E = pygame.image.load("mouth/E.png").convert()
+F = pygame.image.load("mouth/F.png").convert()
+G = pygame.image.load("mouth/G.png").convert()
+H = pygame.image.load("mouth/H.png").convert()
+X = pygame.image.load("mouth/X.png").convert()
 
 os.system ("/home/se101/rhubarb-lip-sync/rhubarb/rhubarb -o output.json -f json -r pocketSphinx meme.wav")
 
@@ -24,8 +38,13 @@ f.close()
 
 pygame.mixer.music.play()
 start = time.time()
+value = 0
 while pygame.mixer.music.get_busy():
-    pass
+
+    for i in timing['mouthCues']:
+        while i['start'] < start:
+            pass
+        screen.blit(exec(i['value']), (0, 0))
 end = time.time()
 pygame.mixer.music.unload()
 os.remove("output.json")
